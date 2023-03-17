27d1a3cbc9825c84d59a811c4dba983df7baa9e7
Ryan Esmores <ryanesmores07@gmail.com>
2023-03-17 16:24:04 +0900

    Refactor code to Composition

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("hi there");
+import axios from "axios";
+import { User } from "../models/User";
+
+const user = new User({});
+
+user.events.on("click", () => console.log("it worked"));
+user.events.trigger("click");
